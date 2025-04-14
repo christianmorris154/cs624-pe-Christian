aab5ea20037caf307e05e1b6096d2456bb75099c
christianmorris154 <82065858+christianmorris154@users.noreply.github.com>
2025-04-14 15:32:00 -0700

    Add files via upload

---
+++ my-mobile-app/App.js
@@ -0,0 +1,22 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Christian Morris</Text>
+      <Text>MSCS</Text>
+      <Text>City University of Seattle</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#ffff00',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
